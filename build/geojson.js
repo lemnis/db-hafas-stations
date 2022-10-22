e2a5740cad78e336a8efaf2dfffdcb07fc1df9de
Lisa Martens <lemnis.martens@gmail.com>
2022-10-22 17:30:07 +0200

    Add geojson

---
+++ build/geojson.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const ndjson = require("ndjson");
+const path = require("path");
+
+const filePath = process.argv[process.argv.length - 1];
+const output = path.join(
+	path.dirname(filePath),
+	path.parse(filePath).name + ".geojson"
+);
+
+const features = [];
+
+fs.createReadStream(filePath)
+	.pipe(ndjson.parse())
+	.on("data", (properties) =>
+		features.push({
+			type: "Feature",
+			geometry: {
+				coordinates: [properties.location.longitude, properties.location.latitude],
+				type: "Point",
+			},
+			properties,
+		})
+	)
+	.on("end", (e) =>
+		fs.writeFileSync(
+			output,
+			JSON.stringify(
+				{
+					type: "FeatureCollection",
+					features,
+				},
+				null,
+				2
+			)
+		)
+	);
